8c808dc6a1f715ef1b5d177a67943179cc8498d6
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 05:26:16 +0530

    Add sout function

--- basics/first.java
+++ basics/first.java
@@ -1,4 +1,9 @@
 package Java_Program.basics;
 
 public class first {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+        System.out.println("Sandip University"); //println-> ln  is use for next line
+        System.out.print("Hi college");
+    }
 }
