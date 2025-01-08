77d89bb4b6634a1b6f80bb7d98b34d69a817658f
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 05:40:53 +0530

    New Folder

---
+++ basics/print_number.java
@@ -0,0 +1,10 @@
+package Java_Program.basics;
+
+public class print_number {
+    public static void main(String[] args){
+        System.out.println("4");     // print as text
+        System.out.println(4);       // print as value also 4
+        System.out.println("4+3+9"); // print text
+        System.out.println(4+3+9);   // print value
+    }
+}
