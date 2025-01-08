8f5b979ab0e8dab331fc436c66bf03586ce84335
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 09:53:47 +0530

    Percentage Mark

---
+++ basics/student_mark.java
@@ -0,0 +1,16 @@
+package Java_Program.basics;
+
+public class student_mark {
+    public static void main(String[] args) {
+
+        int sub1=90;
+        int sub2=91;
+        int sub3=93;
+        int sub4=93;
+        int sub5=94;
+
+        //calculate percentage mark
+        double per=(sub1+sub2+sub3+sub4+sub5)/5.0;
+        System.out.println("Percentage Marks: "+per+"%");
+    }
+}
