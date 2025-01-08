b1c285fb1d825c9b084f43c62860dd043077eb35
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 09:27:26 +0530

    Basic print

--- basics/output.java
+++ basics/output.java
@@ -1,6 +1,6 @@
 package Java_Program.basics;
 
-public class first {
+public class output {
     public static void main(String[] args) {
         System.out.print("Hello world");
         System.out.println("Sandip University"); //println-> ln  is use for next line
