cc20237133276bcc8418caa285215494de3f5672
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 09:58:05 +0530

    Area of Circle

---
+++ basics/areaCircle.java
@@ -0,0 +1,12 @@
+package Java_Program.basics;
+
+public class areaCircle {
+    public static void main(String[] args) {
+
+        double radius=5;
+        double pi=3.14157;
+        double area=pi*radius*radius;
+
+        System.out.printf("Area of Circle: %.2f",area);
+    }
+}
