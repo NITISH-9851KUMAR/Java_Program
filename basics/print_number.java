9621583ede0427ce88ad2d1c550388bd0a83060c
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 09:28:10 +0530

    Print Number

--- basics/print_number.java
+++ basics/print_number.java
@@ -2,6 +2,7 @@ package Java_Program.basics;
 
 public class print_number {
     public static void main(String[] args){
+
         System.out.println("4");     // print as text
         System.out.println(4);       // print as value also 4
         System.out.println("4+3+9"); // print text
