92476bac0899e344b6eec65a8b2df8d7ec2fb2f6
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 11:18:41 +0530

    Type Castring

---
+++ basics/typeCasting.java
@@ -0,0 +1,12 @@
+package Java_Program.basics;
+
+public class typeCasting {
+    public static void main(String[] args) {
+
+        char ch='a';
+        int x=(int)ch; //Type casting
+        //or int x=ch
+        System.out.println(ch);
+        System.out.println(x);
+    }
+}
