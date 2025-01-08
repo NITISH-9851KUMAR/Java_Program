52837c72580fe99e89d0e773161fea6974a572d8
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 05:48:39 +0530

    new file variable

---
+++ basics/variable.java
@@ -0,0 +1,27 @@
+package Java_Program.basics;
+
+public class variable{
+    public static void main(String[] args){
+
+        int x; //declare a variable
+        x=20;
+        //or
+        int y=30; //also declare a variable
+        System.out.println(x);
+        System.out.println(y);
+
+        x=40;
+        y=90;
+        System.out.println(x);
+        System.out.println(y);
+
+        x=x+90;
+        y=y+130;
+        System.out.println(x);
+        System.out.println(y);
+
+        System.out.println(x+200);
+        System.out.println(y+300);
+
+    }
+}
