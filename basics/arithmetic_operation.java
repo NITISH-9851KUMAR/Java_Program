d0e1dc72125b0c88824e5be75a20daa40262e242
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 05:52:36 +0530

    file arithmetic operation

---
+++ basics/arithmetic_operation.java
@@ -0,0 +1,15 @@
+package Java_Program.basics;
+
+public class arithmetic_operation {
+    public static void main(String[] args) {
+
+        // Arithmetic operation
+        int x=5;
+        int y=2;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
