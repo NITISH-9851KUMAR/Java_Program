7a95fae3b256bdc25867dd7814c05f610206126a
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 09:13:13 +0530

    Different data types

---
+++ basics/dataType.java
@@ -0,0 +1,29 @@
+package Java_Program.basics;
+
+public class dataType {
+    public static void main(String[] args) {
+
+        byte b=10;
+        short s=90;
+        int i=14055;
+        long l=220205131004l;
+
+        char ch='n';
+        String str="College";
+
+        float f=3.34f;
+        double d=34.343433d;
+
+        boolean flag=true;
+
+        System.out.println("byte :"+b);
+        System.out.println("short :"+s);
+        System.out.println("int :"+i);
+        System.out.println("long :"+l);
+        System.out.println("char :"+ch);
+        System.out.println("String :"+str);
+        System.out.println("float :"+f);
+        System.out.println("double :"+d);
+        System.out.println("boolean :"+flag);
+    }
+}
