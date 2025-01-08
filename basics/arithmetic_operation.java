2befea55f3c83b3059d9f92d4c812368e544f007
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-08 09:25:36 +0530

    Arithmetic Operation

--- basics/arithmetic_operation.java
+++ basics/arithmetic_operation.java
@@ -3,10 +3,10 @@ package Java_Program.basics;
 public class arithmetic_operation {
     public static void main(String[] args) {
 
-        // Arithmetic operation
         int x=5;
         int y=2;
 
+        // Arithmetic operation
         System.out.println(x+y);
         System.out.println(x-y);
         System.out.println(x*y);
