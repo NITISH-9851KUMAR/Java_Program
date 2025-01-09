539c72601abfe1db36815c0c1325d64be61b074c
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-09 06:47:33 +0530

    Basic of if else

---
+++ if_else/basic.java
@@ -0,0 +1,15 @@
+package Java_Program.if_else;
+
+public class basic {
+    public static void main(String[] args) {
+
+        int x=10;
+
+        if (x%2==0) {
+            System.out.println("Even Number");
+        }
+        else {
+            System.out.println("Odd Number");
+        }
+    }
+}
