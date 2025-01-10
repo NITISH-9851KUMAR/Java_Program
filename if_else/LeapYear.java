bf84a779d145d6953218902f7747cd744692e6ea
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-10 23:03:28 +0530

    Leap Year

--- if_else/LeapYear.java
+++ if_else/LeapYear.java
@@ -3,7 +3,7 @@
 package Java_Program.if_else;
 
 import java.util.Scanner;
-public class leapYear {
+public class LeapYear {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
