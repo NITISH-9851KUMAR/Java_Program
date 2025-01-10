ac9c7ff69659a09a39d3c4b23c7dfae6f6c11b42
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-10 22:59:53 +0530

    Absolute value of Integer

--- if_else/AbsoluteValue.java
+++ if_else/AbsoluteValue.java
@@ -3,7 +3,7 @@
 package Java_Program.if_else;
 
 import java.util.Scanner;
-public class absoluteValue {
+public class AbsoluteValue {
     public static void main(String[] args) {
            Scanner sc=new Scanner(System.in);
 
