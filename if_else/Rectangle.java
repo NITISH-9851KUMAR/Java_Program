d3e34099bc5192b93a948ec6761a7452456b915d
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-10 22:22:03 +0530

    Area and Perimeter of Rectangle

---
+++ if_else/Rectangle.java
@@ -0,0 +1,28 @@
+/** Given the length and breadth of a rectangle, write a program to find whether the area of the rectangle is greater than its perimeter.*/
+package Java_Program.if_else;
+
+import java.util.Scanner;
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter Length: ");
+        double length=sc.nextDouble();
+
+        System.out.print("Enter breadth: ");
+        double breadth=sc.nextDouble();
+
+        double area=length*breadth;
+        double perimeter=2*(length+breadth);
+
+        System.out.printf("Area: %.2f\n",area);
+        System.out.printf("Perimeter: %.2f\n",perimeter);
+        if(area>perimeter){
+            System.out.println("Area is Greater than perimeter");
+        }
+      else {
+            System.out.println("Area is less than perimeter");
+        }
+
+    }
+}
