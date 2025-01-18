4aaab518a36fc14556c362bc4a4520eb3e42727f
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-18 22:55:07 +0530

    Print opposite loop

---
+++ loops/Print100to1.java
@@ -0,0 +1,10 @@
+package Java_Program.loops;
+
+public class Print100to1 {
+    public static void main(String[] args) {
+
+        for(int i=100;i>0;i--){
+            System.out.print(i+"\t");
+        }
+    }
+}
