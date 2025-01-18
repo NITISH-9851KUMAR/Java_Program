b89e7f2469ff92b335fbad40e45fa6862331fa6c
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-18 22:32:46 +0530

    Divisible by 3

--- loops/DivBy3.java
+++ loops/DivBy3.java
@@ -4,6 +4,7 @@ package Java_Program.loops;
 public class DivBy3 {
     public static void main(String[] args) {
 
+        System.out.println("Divisible by 3: ");
         for(int i=1;i<=100;i++){
             if(i%3==0) System.out.print(i+"\t");
         }
