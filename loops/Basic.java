31c1f1a3f870a5129269a1c5b11879ddc06e204a
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-18 22:14:11 +0530

    Basic of Loops

---
+++ loops/Basic.java
@@ -0,0 +1,10 @@
+package Java_Program.loops;
+
+public class Basic{
+    public static void main(String[] args) {
+
+        for(int i=0;i<10;i++){
+            System.out.println("हर हर महादेव");
+        }
+    }
+}
