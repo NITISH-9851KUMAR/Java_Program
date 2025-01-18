5abdbea886d374daccb50ecfa747bb2643ffce6f
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-18 22:22:35 +0530

    Even Number from 1 to 100

---
+++ loops/EvenNumber.java
@@ -0,0 +1,10 @@
+package Java_Program.loops;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        System.out.println("Even Number ");
+        for(int i=2;i<=100;i+=2){
+            System.out.print(i+"\t");
+        }
+    }
+}
