4e21749dbf0516395f3079adb06a3ce6d9be6b3f
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-20 03:38:47 +0530

    While Loops

---
+++ loops/WhileLoops.java
@@ -0,0 +1,12 @@
+package Java_Program.loops;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+
+        int i=1;
+        while(i<=12){
+            System.out.printf("%d ",i);
+            i++;
+        }
+    }
+}
