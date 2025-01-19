b32cdc55e9ed779460fb681fcec07c2d360726ef
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-20 04:15:18 +0530

    Sum of Digits

---
+++ loops/SumOfNumber.java
@@ -0,0 +1,23 @@
+/** Write a program to print sum of digit of a given number*/
+
+package Java_Program.loops;
+
+import java.util.Scanner;
+public class SumOfNumber {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter A Number: ");
+        int num= sc.nextInt();
+
+        int sum= 0;
+        int lastDigit= 0;
+        while(num!=0){
+            lastDigit= num%10;
+            num/=10;
+            sum+=lastDigit;
+        }
+        System.out.println("Sum Of Digits: "+sum);
+
+    }
+}
