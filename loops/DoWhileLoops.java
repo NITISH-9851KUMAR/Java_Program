7a16eeef5fe1c7fc9e2c211e5b9cbe8c10abd543
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-20 03:40:24 +0530

    Do While Loops

---
+++ loops/DoWhileLoops.java
@@ -0,0 +1,12 @@
+package Java_Program.loops;
+
+public class DoWhileLoops {
+    public static void main(String[] args) {
+
+        int i=1;
+        do{
+            System.out.print(i+" ");
+            i++;
+        }while(i!=11);
+    }
+}
