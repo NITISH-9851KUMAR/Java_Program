cafd2c911321636c4332bffd0b6cd77255205e8d
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-21 02:02:03 +0530

    Product of digit of Given Number

---
+++ loops/ProductOfDigits.java
@@ -0,0 +1,24 @@
+/** Write a program to print the product of digit of a given Number.*/
+
+package Java_Program.loops;
+
+import java.util.Scanner;
+
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter a Number: ");
+        int num= sc.nextInt();
+        int product= 1;
+        int lastDigit= 0;
+
+        while(num!=0){
+            lastDigit= num%10;
+            product*= lastDigit;
+            num/= 10;
+        }
+        System.out.println("Product of Digits: "+product);
+
+    }
+}
