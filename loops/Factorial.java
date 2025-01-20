94548e9d21fdf206a5b0520ffb190a28101ad20a
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-21 02:51:04 +0530

    Factorial of Number

---
+++ loops/Factorial.java
@@ -0,0 +1,34 @@
+package Java_Program.loops;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter a Number: ");
+        int num= sc.nextInt();
+
+        if(num==0){
+            System.out.println("0!: 0");
+        }
+
+        int fact=1;
+        for(int i=num;i>=1;i--){
+            if(i==num){
+                System.out.print(num+"!: "+num+"*");
+                fact*=i;
+                continue;
+            }
+            else if(i==1) {
+                System.out.print("1");
+                System.out.printf("\n%d!: %d",num,fact);
+                break;
+            }
+            System.out.print(i+"*");
+            fact*=i;
+        }
+
+
+    }
+}
