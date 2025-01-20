4da039ff9cf02f85cb98cb281b3c471003c72bb7
NITISH-9835KUMAR <nitish983577@gmail.com>
2025-01-21 03:20:26 +0530

    Factorial of First N Number

---
+++ loops/FactNNumber.java
@@ -0,0 +1,20 @@
+/** Print the factorials of first 'n' numbers.*/
+
+package Java_Program.loops;
+
+import java.util.Scanner;
+
+public class FactNNumber {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter a Number: ");
+        int num= sc.nextInt();
+        int fact=1;
+        for(int i=1;i<=num;i++){
+            fact*=i;
+            System.out.println(i+"!: "+fact);
+        }
+
+    }
+}
