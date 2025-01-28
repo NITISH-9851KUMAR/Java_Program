39967c5f5061be36f4b48a41187c9eb10dfe29d3
Nitish Kumar <nitish983577@gmail.com>
2025-01-28 19:50:08 +0530

    Basics of Pattern Printing

--- patternPrinting/Basic.java
+++ patternPrinting/Basic.java
@@ -1,2 +1,19 @@
-package Java_Program.patternPrinting;public class Basic {
+//*****
+//*****   print this Pattern
+//*****
+
+package Java_Program.patternPrinting;
+
+import java.sql.SQLOutput;
+
+public class Basic {
+    public static void main(String[] args) { // Execute three time basically work for print line
+
+        outerLoop: for(int i= 0; i<3; i++){
+        innerLoop:     for(int j= 0; j<5; j++) System.out.print("*");
+
+                 System.out.println(); // Next Line
+
+        }
+    }
 }
