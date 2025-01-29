4525216ba72939164d9c4175132aabd55b930286
Nitish Kumar <nitish983577@gmail.com>
2025-01-29 21:54:01 +0530

    Alphabet Pattern

--- patternPrinting/AlphabetSquare.java
+++ patternPrinting/AlphabetSquare.java
@@ -1,7 +1,7 @@
 /*
 A B C D
 A B C D
-A B C D  Pattern
+A B C D  Pattern if j== 4 , also if j== 3 then print only three column
 A B C D
 */
 
@@ -9,12 +9,13 @@ package Java_Program.patternPrinting;
 
 public class AlphabetSquare {
     public static void main(String[] args) {
+
         for(int i= 1; i<5; i++){
-            int val= 65;
             for( int j= 1; j<=4; j++){
+                int val= 64;
+                val+= j;
                 char ch= (char)val;
                 System.out.print(ch+" ");
-                val++;
             }
             System.out.println();
         }
