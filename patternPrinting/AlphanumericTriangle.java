ed31b4df5327b8deca93ec82ec7eda656f94deca
Nitish Kumar <nitish983577@gmail.com>
2025-01-29 22:15:12 +0530

    Alphanumeric Triangle

---
+++ patternPrinting/AlphanumericTriangle.java
@@ -0,0 +1,21 @@
+/*
+        1
+        A B
+        1 2 3
+        A B C D
+        1 2 3 4 5
+ */
+
+package Java_Program.patternPrinting;
+
+public class AlphanumericTriangle {
+    public static void main(String[] args) {
+        for(int i= 1; i<=5; i++){
+            for( int j= 1; j<= i; j++){
+                if(i%2 != 0)System.out.print(j+" ");
+                else System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
