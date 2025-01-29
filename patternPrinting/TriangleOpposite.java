65046fa9ad8fd39c96b469b2c357812c33fa9352
Nitish Kumar <nitish983577@gmail.com>
2025-01-29 22:34:19 +0530

    Opposite Triangle

---
+++ patternPrinting/TriangleOpposite.java
@@ -0,0 +1,23 @@
+/*
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+ */
+
+package Java_Program.patternPrinting;
+
+public class TriangleOpposite {
+    public static void main(String[] args) {
+
+        byte n= 5;
+        for(int i= 1;i<=n; i++){
+            for(int j= 1; j <= n+1-i;  j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
