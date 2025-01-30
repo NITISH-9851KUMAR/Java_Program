063b40881c720cdfced4901191f22f45fb5b2907
Nitish Kumar <nitish983577@gmail.com>
2025-01-30 19:42:16 +0530

    Constructor Argument

--- oop/ConstructorPassed.java
+++ oop/ConstructorPassed.java
@@ -5,17 +5,28 @@ public class ConstructorPassed {
         String name,course;
         int prn;
         float fee;
-        Student(int prn, String name, String course){
+        Student(Student s){ //Parameter as a Constructor
+            name= s.name;
+            prn= s.prn;
+            course= s.course;
+            fee= s.fee;
+        }
+        Student(int prn, String name, String course,float fee){
             this.prn= prn;
             this.name= name;
             this.course= course;
-        }
-        Student(int prn, String name, String course, float fee){
-            this(prn, name, course); //reusing constructor
             this.fee= fee;
+        }
+
+        void display(){
+            System.out.println("Name: "+name+"\nRoll No: "+prn+"\nCourse: "+course);
+            if(fee !=0.0f ) System.out.println("Fee: "+fee);
+            System.out.println();
         }
     }
     public static void main(String[] args) {
-
+       Student s1= new Student(04,"Nitish Kumar","B.Tech",111000.0f);
+       Student s2= new Student(s1); //Passed s1 object as argument
+       s2.display();
     }
 }
