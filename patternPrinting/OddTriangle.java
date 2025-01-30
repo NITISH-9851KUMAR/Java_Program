7a49abd160ca186270d64027b51a1eef68d5bdc3
Nitish Kumar <nitish983577@gmail.com>
2025-01-30 15:01:04 +0530

    Odd Triangle

--- patternPrinting/OddTriangle.java
+++ patternPrinting/OddTriangle.java
@@ -1,2 +1,27 @@
-package Java_Program.patternPrinting;public class OddTriangle {
+/*
+        1
+        1 3
+        1 3 5
+        1 3 5 7
+        1 3 5 7 9
+*/
+
+package Java_Program.patternPrinting;
+
+import java.util.Scanner;
+
+public class OddTriangle {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("N: ");
+        byte n= sc.nextByte();
+        for(int i=1; i<=n;i++){
+            for(int j= 1; j<=i; j++){
+                System.out.print((2*j-1)+" ");
+            }
+            System.out.println();
+        }
+
+    }
 }
