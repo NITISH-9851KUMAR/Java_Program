5a16c078bc606bc7102f291f0b70425ab441631d
Nitish Kumar <nitish983577@gmail.com>
2025-01-30 18:45:41 +0530

    Create Object

---
+++ oop/ObjectCreate.java
@@ -0,0 +1,10 @@
+package Java_Program.oop;
+
+public class ObjectCreate {
+    public static class Room{
+        int length, width;
+    }
+    public static void main(String[] args) {
+        Room room= new Room(); //Create object of Room class
+    }
+}
