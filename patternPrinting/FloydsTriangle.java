128c4ad9491e1cb24239dcf5672a5d64aa478d89
Nitish Kumar <nitish983577@gmail.com>
2025-01-30 15:00:40 +0530

    Floyd's Triangle

--- patternPrinting/FloydsTriangle.java
+++ patternPrinting/FloydsTriangle.java
@@ -1,2 +1,29 @@
-package Java_Program.patternPrinting;public class FloydsTriangle {
+/*
+        1
+        2 3
+        4 5 6
+        7 8 9 10
+        11 12 13 14 15
+*/
+
+package Java_Program.patternPrinting;
+
+import java.util.Scanner;
+
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("N: ");
+        byte n= sc.nextByte();
+        byte a= 1;
+        for(int i=1; i<=n;i++){
+            for(int j= 1; j<=i; j++){
+                System.out.print(a+" ");
+                a+= 1;
+            }
+            System.out.println();
+        }
+
+    }
 }
