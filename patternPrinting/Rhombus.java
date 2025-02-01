45e944c1dc61af2d42ba53aaa87eef897dff133d
Nitish Kumar <nitish983577@gmail.com>
2025-02-01 14:44:02 +0530

    Rhombus Patter

--- patternPrinting/Rhombus.java
+++ patternPrinting/Rhombus.java
@@ -17,13 +17,26 @@ public class Rhombus {
 
         System.out.print("N: ");
         byte n= sc.nextByte();
-        int col= (n*2-1);
-        for(int i=0; i<n;i++){
-            for(int j= 0; j< col; j++){
-                if( (i+j) >= col/2 && (i+j)<=col-1  )
-                    System.out.print("* ");
-                else System.out.print("  ");
-                }
+//        int col= (n*2-1);
+//        for(int i=0; i<n;i++){
+//            for(int j= 0; j< col; j++){
+//                if( (i+j) >= col/2 && (i+j)<=col-1  )
+//                    System.out.print("* ");
+//                else System.out.print("  ");
+//                }
+//            System.out.println();
+//        }
+
+        //Method Number 2nd
+        int nsp= n-1;
+        int nst= n;
+        for(int i= 0; i<n;i++){
+            for(int a= 1; a<=nsp; a++) //Number of space
+                System.out.print("  ");
+            for(int b= 1; b<=nst;b++)
+                System.out.print("* ");
+
+            nsp--;
             System.out.println();
         }
 
