698fd36ba25d398077ca7ab53aea6dd9bff2f2ce
Nitish Kumar <nitish983577@gmail.com>
2025-02-01 14:58:21 +0530

    Star Pyramid

---
+++ patternPrinting/StarPyramid.java
@@ -0,0 +1,31 @@
+/*
+
+          *
+        * * *
+      * * * * *
+    * * * * * * *
+
+ */
+
+package Java_Program.patternPrinting;
+
+import java.util.Scanner;
+
+public class StarPyramid {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("N: ");
+        byte n= sc.nextByte();
+
+        for(int i=1; i<=n;i++){
+            for(int a= 1; a<=n-i; a++) // Space
+                System.out.print("  ");
+            for(int b= 1; b<= (2*i-1); b++) // Star
+                System.out.print("* ");
+
+            System.out.println();
+        }
+
+    }
+}
