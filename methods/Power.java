3f5958ab01fb7000f83886b881aeed6b9c74299d
Nitish Kumar <nitish983577@gmail.com>
2025-02-02 18:41:08 +0530

    Power of Number

--- methods/Power.java
+++ methods/Power.java
@@ -1,2 +1,15 @@
-package Java_Program.methods;public class Power {
+package Java_Program.methods;
+public class Power {
+    public static int power(int a, int b){
+        int pow= 1;
+        for(int i= 1; i<=b; i++)
+            pow*= a;
+        return pow;
+    }
+    public static void main(String[] args) {
+        int a= 5;
+        int b= 2;
+        int pow= power(a,b);
+        System.out.printf("%d power %d: %d",a,b,pow);
+    }
 }
---
+++ recursion/Fibonacci.java
@@ -0,0 +1,2 @@
+package Java_Program.recursion;public class Fibbonacci {
+}
