c7f6ce3d47869f20dd94708ed8b475a183b63f35
Nitish Kumar <nitish983577@gmail.com>
2025-02-02 19:01:28 +0530

    Basics of methods

---
+++ methods/Basics.java
@@ -0,0 +1,11 @@
+package Java_Program.methods;
+
+public class Basics {
+    public static void greet(){
+        System.out.println("Good Morning!");
+        System.out.println("I am called vai main Method!");
+    }
+    public static void main(String[] args) {
+        greet(); //Function call
+    }
+}
