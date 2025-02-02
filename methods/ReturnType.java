91a02ee73722c527c67f10fb3637089d24c2190e
Nitish Kumar <nitish983577@gmail.com>
2025-02-02 19:09:05 +0530

    Return Type Function

---
+++ methods/ReturnType.java
@@ -0,0 +1,13 @@
+package Java_Program.methods;
+
+public class ReturnType {
+    public static void main(String[] args) {
+        int x= function();
+        System.out.println(x);
+
+    }
+
+    private static int function() { //This function return a value
+        return 1;
+    }
+}
