1f1239c095e2998784aedaa066e998d75b070642
Nitish Kumar <nitish983577@gmail.com>
2025-02-05 21:59:25 +0530

    Final Variable

--- oop/finalKeyword/FinalVariable.java
+++ oop/finalKeyword/FinalVariable.java
@@ -1,2 +1,17 @@
-package Java_Program.oop.finalKeyword;public class FinalVariable {
+package Java_Program.oop.finalKeyword;
+
+public class FinalVariable {
+    public static class Base{
+        final String schoolName= "Sandip University";
+        public Base(){
+            System.out.println("College Name: "+schoolName);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Base base= new Base();
+        // base.schoolName= "Sandip College"; It cannot be modified because it final in Base Class
+    }
 }
