d732b858a0fd2f27af539029eff329a45c8ead35
Nitish Kumar <nitish983577@gmail.com>
2025-02-05 21:59:09 +0530

    Final Method

--- oop/finalKeyword/FinalMethod.java
+++ oop/finalKeyword/FinalMethod.java
@@ -1,2 +1,19 @@
-package Java_Program.oop.finalKeyword;public class FinalMethod {
+package Java_Program.oop.finalKeyword;
+
+public class FinalMethod {
+    public static abstract class Base{
+        final void fun(){
+            System.out.println("Final fun() is called! ");
+        }
+    }
+    public static class Derived extends Base{
+//        void fun(){ //fun() function can not be overridden because it is final in Base class
+//            System.out.println("Final fun() is called! ");
+//        }
+
+    }
+    public static void main(String[] args) {
+        Base b= new Derived();
+        b.fun();
+    }
 }
