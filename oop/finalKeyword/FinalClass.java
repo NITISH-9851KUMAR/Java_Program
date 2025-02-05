3eebdf8db0a8339de35a517a33918685b59327ff
Nitish Kumar <nitish983577@gmail.com>
2025-02-05 21:58:50 +0530

    Final Class

---
+++ oop/finalKeyword/FinalClass.java
@@ -0,0 +1,15 @@
+package Java_Program.oop.finalKeyword;
+
+public class FinalClass {
+    public static final class Bike{
+        int break1;
+    }
+
+//    public static class Honda extends Bike{ // Bike class can not inherit because it is final class
+//
+//    }
+    public static void main(String[] args) {
+
+        Bike honda= new Bike(); // Final class can be instantiated 
+    }
+}
---
+++ oop/finalKeyword/FinalVariable.java
@@ -0,0 +1,2 @@
+package Java_Program.oop.finalKeyword;public class FinalVariable {
+}
