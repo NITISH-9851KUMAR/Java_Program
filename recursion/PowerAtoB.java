db06a1dcb018a89e6e12bc0ef70ab2ac70af2d09
Nitish Kumar <nitish983577@gmail.com>
2025-02-07 19:43:47 +0530

    power of a to b

--- recursion/PowerAtoB.java
+++ recursion/PowerAtoB.java
@@ -1,13 +1,22 @@
 package Java_Program.recursion;
 
+import java.util.Scanner;
+
 public class PowerAtoB {
     static int power(int a, int b){
-        if(b==1) return a;
+        if(b==0) return 1;
         int pow= a * power(a,b-1);
         return pow;
     }
     public static void main(String[] args) {
-        int pow= power(5, 2);
+        java.util.Scanner sc= new Scanner(System.in);
+        System.out.print("Enter base: ");
+        byte a= sc.nextByte();
+        System.out.print("Enter power: ");
+        byte b= sc.nextByte();
+
+
+        int pow= power(a, b);
         System.out.println(pow);
     }
 }
