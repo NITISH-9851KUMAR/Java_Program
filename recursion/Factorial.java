2b1c37b149bef8e788dfd0bc5322808a10e2cf5f
Nitish Kumar <nitish983577@gmail.com>
2025-02-07 19:04:48 +0530

    Factorial of Number

--- recursion/Factorial.java
+++ recursion/Factorial.java
@@ -2,9 +2,8 @@ package Java_Program.recursion;
 
 public class Factorial {
     public static int factorial(int x){
-        if(x==0) return 1;
-        else
-            return x*(factorial(x-1));
+        if(x==1 || x==0) return 1; //It is base case it is very important
+        return x*(factorial(x-1));
     }
     public static void main(String[] args) {
         int x= 5;
