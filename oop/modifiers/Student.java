6902df2ef211b6a27f9be46efaecc94aaba92d79
Nitish Kumar <nitish983577@gmail.com>
2025-02-08 12:28:59 +0530

    use for modifiers

--- oop/modifiers/Student.java
+++ oop/modifiers/Student.java
@@ -1,2 +1,7 @@
-package Java_Program.oop.modifiers;public class Student {
+package Java_Program.oop.modifiers;
+
+class Student {
+    int prn= 31004;
+    private String password= "NITIYA@9851prish";
+    protected String email= "nitiya9851@gamil.com";
 }
