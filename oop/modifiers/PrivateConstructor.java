67e4c08821b8f78f7f6c6390404068db772c2f26
Nitish Kumar <nitish983577@gmail.com>
2025-02-08 12:26:40 +0530

    PrivateConstructor.java

--- oop/modifiers/PrivateConstructor.java
+++ oop/modifiers/PrivateConstructor.java
@@ -1,2 +1,17 @@
-package Java_Program.oop.modifiers;public class PrivateConstructor {
+package Java_Program.oop.modifiers;
+
+class StudentMe{
+    //if we create a private constructor of any class then can't create the object of this class
+    private StudentMe(){ //Default constructor is private, so we cannot create the object of default type
+        System.out.println("Hello");
+    }
+    StudentMe(String name){
+        System.out.println(name);
+    }
+}
+public class PrivateConstructor {
+    public static void main(String[] args) {
+//        StudentMe s1= new StudentMe(); define private constructor in class
+        StudentMe s2= new StudentMe("Nitish");
+    }
 }
