5594ed7a37561e5f4d9f22d04ccf0ade15701258
Nitish Kumar <nitish983577@gmail.com>
2025-02-08 10:55:12 +0530

    Public Modifiers

--- oop/modifiers/Public.java
+++ oop/modifiers/Public.java
@@ -1,2 +1,11 @@
-package Java_Program.oop.modifiers;public class Public {
+package Java_Program.oop.modifiers;
+
+import Java_Program.patternPrinting.Rectangle;
+
+public class Public {
+    public static void main(String[] args) {
+        Rectangle r1= new Rectangle(); // Rectangle is defined in patternPrinting package with public access modifiers
+        System.out.println(r1.width);  // width is defined with public modifier
+       // System.out.println(r1.height); // height did not define with public modifier, so we can not access them
+    }
 }
