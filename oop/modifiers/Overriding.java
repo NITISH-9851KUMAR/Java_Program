a635e2785fa8bedc783177a8e53d6960206788dd
Nitish Kumar <nitish983577@gmail.com>
2025-02-08 12:24:27 +0530

    Overriding method

--- oop/modifiers/Overriding.java
+++ oop/modifiers/Overriding.java
@@ -1,2 +1,33 @@
-package Java_Program.oop.modifiers;public class Overriding {
+package Java_Program.oop.modifiers;
+
+class Base{
+    private void privateBase(){
+        System.out.println("Main hu privateBase class!");
+    }
+    protected void protectedBase(){
+        System.out.println("Main hu protectedBase class!");
+    }
+    public void publicBase(){
+        System.out.println("Main hu publicBase class!");
+    }
+}
+class Sub extends Base{
+    private void privateBase (){ // we cannot override this
+       System.out.println("Main hu privateSub class!");
+    }
+    protected void protectedBase(){
+        System.out.println("Main hu protectedSub class!");
+    }
+    public void publicBase(){
+        System.out.println("Main hu publicSub class!");
+    }
+}
+public class Overriding {
+    public static void main(String[] args) {
+        Base b= new Sub();
+//        b.privateBase(); // can not override to bas class
+        b.protectedBase();
+        b.publicBase();
+
+    }
 }
