b29a49727fd32ba6a736f6c74b671aa92ee92b59
Nitish Kumar <nitish983577@gmail.com>
2025-02-08 11:12:45 +0530

    private modifiers

--- oop/modifiers/Private.java
+++ oop/modifiers/Private.java
@@ -1,2 +1,10 @@
-package Java_Program.oop.modifiers;public class Private {
+package Java_Program.oop.modifiers;
+
+
+public class Private {
+    public static void main(String[] args) {
+        Student s1= new Student();
+        System.out.println(s1.prn); // prn can be accessible. It is default
+        //System.out.println(s1.password); // password is defined with private, it can be accessible with same class
+    }
 }
