102cfb0e63bbdda45f4d0476f66ba0f597e13251
Nitish Kumar <nitish983577@gmail.com>
2025-02-08 19:48:28 +0530

    Print sum 1 to n return  type

--- recursion/SumReturnType.java
+++ recursion/SumReturnType.java
@@ -1,2 +1,12 @@
-package Java_Program.recursion;public class SumReturnType {
+package Java_Program.recursion;
+
+public class SumReturnType {
+    public static void main(String[] args) {
+
+        System.out.println(sum(10));
+    }
+    static int sum(int n){
+        if(n== 1) return 1;
+        return n+ sum(n-1);
+    }
 }
