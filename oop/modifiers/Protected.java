c637ae6e2882ea67c5049531fde4b60d68531394
Nitish Kumar <nitish983577@gmail.com>
2025-02-08 12:27:51 +0530

    Protected Modifier

--- oop/modifiers/Protected.java
+++ oop/modifiers/Protected.java
@@ -1,2 +1,16 @@
-package Java_Program.oop.modifiers;public class Protected {
+package Java_Program.oop.modifiers;
+import Java_Program.patternPrinting.Rectangle;
+
+class Class1 extends Rectangle{
+    // Class1 extends the property of Rectangle
+    //Class1 can access the area
+    void dis(){
+        System.out.println(area);
+    }
+}
+public class Protected{
+    public static void main(String[] args) {
+        Class1 c1= new Class1();
+        c1.dis();
+    }
 }
