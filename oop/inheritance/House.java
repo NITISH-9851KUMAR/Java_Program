a7cb2d11ccfd39b50c0840377855451ed11bb797
Nitish Kumar <nitish983577@gmail.com>
2025-02-08 12:29:37 +0530

    Use for modifiers

--- oop/inheritance/House.java
+++ oop/inheritance/House.java
@@ -1,2 +1,9 @@
-package Java_Program.oop.inheritance;public class House {
+package Java_Program.oop.inheritance;
+
+class House { // It is used for class Default which is within modifiers package
+    private String hName= "Nitish Kumar"; //Declare with private modifiers, // accessible only same class
+    int prn= 31004;  // Declare with default modifiers , // accessible only same package within any class
+    protected char grade='A'; // Declare with protected modifiers  // accessible only subclass any package
+
+    public String collName= "Sandip University"; // Declare with public modifiers  // accessible anywhere in same project
 }
