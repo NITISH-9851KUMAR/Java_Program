73aa80529385c2670f1a005484348773e441c590
Nitish Kumar <nitish983577@gmail.com>
2025-02-10 19:10:06 +0530

    rint value

---
+++ methods/mathFunction/Rint.java
@@ -0,0 +1,12 @@
+package Java_Program.methods.mathFunction;
+
+public class Rint {
+    public static void main(String[] args) {
+
+        double x= Math.rint(90.23);
+        double y= Math.rint(90.51);
+// rint(x) Returns the truncated value of x
+        System.out.println("rint of 90.23: "+x);
+        System.out.println("rint of 90.51: "+y);
+    }
+}
