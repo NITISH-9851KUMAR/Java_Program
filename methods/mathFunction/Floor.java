1a410599550ad93c069d1a7e6b5cf51809730536
Nitish Kumar <nitish983577@gmail.com>
2025-02-10 19:00:50 +0530

    Rounded down

---
+++ methods/mathFunction/Floor.java
@@ -0,0 +1,11 @@
+package Java_Program.methods.mathFunction;
+
+public class Floor {
+    public static void main(String[] args) {
+
+        double num= Math.floor(23.23);
+//        ceil(x) Returns the largest whole number greater than or equal to x. (Rounded down)
+
+        System.out.println("ceil of 23.23: "+num);
+    }
+}
