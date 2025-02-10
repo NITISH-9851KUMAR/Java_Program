5f33840b375de2c7bc443b0d9736532002be7a2e
Nitish Kumar <nitish983577@gmail.com>
2025-02-10 19:04:58 +0530

    Power function

---
+++ methods/mathFunction/Power.java
@@ -0,0 +1,11 @@
+package Java_Program.methods.mathFunction;
+
+public class Power {
+    public static void main(String[] args) {
+
+        double x= Math.pow(10.63, 3);
+        //pow(x, y) Returns x raised to y (x pow y)
+        System.out.printf("10 to power 3: %.2f",x);
+
+    }
+}
