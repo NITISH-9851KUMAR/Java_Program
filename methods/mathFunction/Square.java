fbda879265b96b648024a40beb0197ebb7692ae0
Nitish Kumar <nitish983577@gmail.com>
2025-02-10 18:45:11 +0530

    Square Root

--- methods/mathFunction/Square.java
+++ methods/mathFunction/Square.java
@@ -5,6 +5,7 @@ public class Square {
 
         System.out.print("Enter Number: ");
         double num= new java.util.Scanner(System.in).nextDouble();
+        //Math.sqrt();
         System.out.printf("Square Root of %.2f: %.2f",num,Math.sqrt(num));
 
     }
