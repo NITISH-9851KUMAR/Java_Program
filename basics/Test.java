3a682f6eb7f5112d2bf64b61507fb2de9d1304db
Nitish Kumar <nitish983577@gmail.com>
2025-02-10 18:36:32 +0530

    Square Root

---
+++ basics/Test.java
@@ -0,0 +1,9 @@
+package Java_Program.basics;
+import Java_Program.patternPrinting.Rectangle;
+public class Test {
+    public static void main(String[] args) {
+        Rectangle r1= new Rectangle();
+        r1.width= 90;
+        System.out.println(r1.width;
+    }
+}
---
+++ methods/mathFunction/Square.java
@@ -0,0 +1,11 @@
+package Java_Program.methods.mathFunction;
+
+public class Square {
+    public static void main(String[] args) {
+
+        System.out.print("Enter Number: ");
+        double num= new java.util.Scanner(System.in).nextDouble();
+        System.out.printf("Square Root of %.2f: %.2f",num,Math.sqrt(num));
+
+    }
+}
