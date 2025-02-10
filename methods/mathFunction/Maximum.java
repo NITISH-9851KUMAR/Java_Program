f4417f495da1a97b6839f72c110446cd663f1c3e
Nitish Kumar <nitish983577@gmail.com>
2025-02-10 18:46:42 +0530

    Maximum Number

--- methods/mathFunction/Maximum.java
+++ methods/mathFunction/Maximum.java
@@ -1,2 +1,17 @@
-package Java_Program.methods.mathFunction;public class Maximum {
+package Java_Program.methods.mathFunction;
+
+import java.util.Scanner;
+public class Maximum {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("a: ");
+        byte num1= sc.nextByte();
+
+        System.out.print("b: ");
+        byte num2= sc.nextByte();
+        //Math.max();
+        System.out.println("Maximum of "+num1+" and "+num2+" :  "+Math.max(num1, num2));
+
+    }
 }
