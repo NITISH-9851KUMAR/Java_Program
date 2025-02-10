ab939d12a99e8943e5944a44033bc86be6565cae
Nitish Kumar <nitish983577@gmail.com>
2025-02-10 18:58:28 +0530

    Rounding Up

---
+++ methods/mathFunction/Ceil.java
@@ -0,0 +1,11 @@
+package Java_Program.methods.mathFunction;
+
+public class Ceil {
+    public static void main(String[] args) {
+
+        double num= Math.ceil(23.23);
+//        ceil(x) Returns the smallest whole number greater than or equal to x. (Rounding up)
+
+        System.out.println("ceil of 23.23: "+num);
+    }
+}
