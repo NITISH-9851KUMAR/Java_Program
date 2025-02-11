f4f0f2d94d4d80fb12da619c537ccadae8469cbc
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 15:35:32 +0530

    Import Types

---
+++ packages/ImportClass.java
@@ -0,0 +1,18 @@
+package Java_Program.packages;
+
+import java.util.*;
+//import all class of util package
+
+import java.lang.String;
+//import single class of lang package
+public class ImportClass {
+    public static void main(String[] args) {
+        //When we need to use it multiple times throughout the program
+        Scanner sc= new Scanner(System.in);
+        int num= sc.nextInt();
+
+        //When we need to use it only once in the whole program
+        int x= new java.util.Scanner(System.in).nextInt();
+
+    }
+}
