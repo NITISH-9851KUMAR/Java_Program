e63bee6b697d39b35bba80dde2283a7de1df3b84
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 22:15:22 +0530

    String Functions

---
+++ strings/Functions.java
@@ -0,0 +1,14 @@
+package Java_Program.strings;
+
+public class Functions {
+    public static void main(String[] args) {
+
+        String str= "College Wallah Channel";
+        //It starts with zero
+        char x= str.charAt(3);
+        int length= str.length();
+
+        System.out.println(x);
+        System.out.println(length);
+    }
+}
