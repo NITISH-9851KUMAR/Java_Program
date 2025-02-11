1be39c8694599db15eeac59c08637a7abeda8d53
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 16:30:52 +0530

    Same Class in different Packages

--- packages/SameClass.java
+++ packages/SameClass.java
@@ -1,2 +1,15 @@
-package Java_Program.packages;public class SameClass {
+package Java_Program.packages;
+
+//import Java_Program.oop.MyClass;
+//import Java_Program.methods.MyClass;  It is not valid we need to use here fully qualified name of class
+public class SameClass {
+    public static void main(String[] args) {
+        //these type of condition where same class name
+        //then use fully qualified name
+        Java_Program.oop.MyClass oopMyClass= new Java_Program.oop.MyClass();
+        oopMyClass.show();
+
+        Java_Program.methods.MyClass methodsMyClass= new Java_Program.methods.MyClass();
+        methodsMyClass.show();
+    }
 }
