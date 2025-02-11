02df908fa14550a54e2b32984aaeed4f2cb09733
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 19:44:36 +0530

    Take input from the user

--- strings/InputTaking.java
+++ strings/InputTaking.java
@@ -1,2 +1,17 @@
-package Java_Program.strings;public class InputTaking {
+package Java_Program.strings;
+
+import java.util.Scanner;
+public class InputTaking {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String str1= sc.next(); //Take input without next line
+
+        System.out.print("Enter String: ");
+        String str2= sc.nextLine(); //Take input with next line
+
+        System.out.println(str1);
+        System.out.println(str2);
+    }
 }
