3b08fe3b4a61a17a3827d639f25eac8bbb0fddd7
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 19:28:07 +0530

    Basics of Strings

--- string/Basic.java
+++
@@ -1,2 +0,0 @@
-package Java_Program.string;public class Basic {
-}
---
+++ strings/Basic.java
@@ -0,0 +1,13 @@
+package Java_Program.strings;
+
+public class Basic {
+    public static void main(String[] args) {
+
+        String str1= "College Wallah"; //Basics declaration of String
+        String str2= new String("Physics Wallah"); //Declaration of String
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+    }
+}
