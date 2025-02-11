894c615ac52d8d159cab05a2ef2208f1b83fb6d6
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 19:15:24 +0530

    Trigonometry Function

---
+++ methods/mathFunction/Trigonometry.java
@@ -0,0 +1,14 @@
+package Java_Program.methods.mathFunction;
+
+public class Trigonometry {
+    public static void main(String[] args) {
+
+        //Returns the sine of the angle x in radians
+        //sin(90) 1 in radians
+        //sin(90) 0.89399 in degree
+
+        System.out.println(Math.sin(90));
+        System.out.println(Math.cos(90));
+        System.out.println(Math.tan(45));
+    }
+}
