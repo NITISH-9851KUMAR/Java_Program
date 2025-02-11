b7f2ae0631d81d0581b4de94f4c39cac80a97841
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 19:07:23 +0530

    Round Function

--- methods/mathFunction/Round.java
+++ methods/mathFunction/Round.java
@@ -1,2 +1,10 @@
-package Java_Program.methods.mathFunction;public class Round {
+package Java_Program.methods.mathFunction;
+
+public class Round {
+    public static void main(String[] args) {
+
+        double num= Math.round(101.4);
+        //Returns the integer closest to the argument
+        System.out.println(num);
+    }
 }
