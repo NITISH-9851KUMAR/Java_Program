8b86b5009759e0c65cfec3649c97d8ae50b9f842
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 15:43:01 +0530

    Import Fully Qualified class Name

---
+++ packages/FullyQualifiedName.java
@@ -0,0 +1,12 @@
+package Java_Program.packages;
+
+public class FullyQualifiedName {
+    public static void main(String[] args) {
+        java.util.Scanner sc= new java.util.Scanner(System.in);
+        //This is fully Qualified class name
+        System.out.print("Number: ");
+        int num;
+        num=sc.nextInt();
+        System.out.println(num);
+    }
+}
