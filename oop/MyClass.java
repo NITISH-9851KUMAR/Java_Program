22367eeff348ba83b6ef522f9d0fd05e73dc059e
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 16:19:29 +0530

    MyClass use for package

--- oop/MyClass.java
+++ oop/MyClass.java
@@ -1,2 +1,7 @@
-package Java_Program.oop;public class MyClass {
+package Java_Program.oop;
+
+public class MyClass {
+    void show(){
+        System.out.println("Hello! It is oop MyClass");
+    }
 }
