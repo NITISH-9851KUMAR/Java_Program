c7e6fb3d4f0b6fc2127cae180d7bd106aa90aa39
Nitish Kumar <nitish983577@gmail.com>
2025-02-11 19:09:44 +0530

    Absolute Value

---
+++ methods/mathFunction/Absolute.java
@@ -0,0 +1,11 @@
+package Java_Program.methods.mathFunction;
+
+public class Absolute {
+    public static void main(String[] args) {
+
+        int x= -5;
+        System.out.println(Math.abs(x));
+        System.out.println(Math.abs(-3.3));
+        System.out.println(Math.abs(3.52));
+    }
+}
