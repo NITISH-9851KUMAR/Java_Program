6017980746edf54abb1b4e7cc4a70a7263472c98
Nitish Kumar <nitish983577@gmail.com>
2025-02-12 22:19:14 +0530

    Basics Functions

--- strings/Functions.java
+++ strings/Functions.java
@@ -3,12 +3,33 @@ package Java_Program.strings;
 public class Functions {
     public static void main(String[] args) {
 
-        String str= "College Wallah Channel";
-        //It starts with zero
-        char x= str.charAt(3);
-        int length= str.length();
+//        String str= "College Wallah Channel";
+//        //It starts with zero
+//        char x= str.charAt(3);
+//        int length= str.length(); // Return the length of string
+//        System.out.println(x);
+//        System.out.println(length);
 
-        System.out.println(x);
-        System.out.println(length);
+        //String.indexOf();
+//        int idx= str.indexOf('W');
+//        System.out.println(idx);
+
+//        compareOf();
+//        String str= "Hello";
+//        String gtr= "Mell0";
+//        System.out.println(str.compareTo(gtr));
+//        lexo-graphically Mello is greater than str M is capital to H through ascii value
+
+//        //contains();
+//        String str= "Sandip University";
+//        System.out.println(str.contains("dip")); //it takes only string value
+
+//        //startWith() endsWith()
+//        String str= "Sandip University";
+//        System.out.println(str.endsWith("Sa"));
+//        System.out.println(str.endsWith("ty"));
+
+
+
     }
 }
