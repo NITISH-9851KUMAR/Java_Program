4ae632ded2e22f4904fee586873685de5abe0171
Nitish Kumar <nitish983577@gmail.com>
2025-02-13 20:19:08 +0530

    Print all substring of String

--- strings/AllSubstring.java
+++ strings/AllSubstring.java
@@ -1,2 +1,16 @@
-package Java_Program.strings;public class AllSubstring {
+package Java_Program.strings;
+
+public class AllSubstring {
+    public static void main(String[] args) {
+
+        String str= "abcd";
+
+        //print all substring of str
+        for(int i= 0; i<= str.length(); i++){
+            for(int j= i+1; j<= str.length(); j++){
+                System.out.print(str.substring(i,j)+" ");
+            }
+        }
+
+    }
 }
