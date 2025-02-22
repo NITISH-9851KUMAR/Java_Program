65257f06726e32445395bd2943f715654a4fdadc
Nitish Kumar <nitish983577@gmail.com>
2025-02-22 11:18:44 +0530

    Compile time error

---
+++ exceptionHandling/CompileTimeError.java
@@ -0,0 +1,13 @@
+package Java_Program.exceptionHandling;
+
+public class CompileTimeError {
+    public static void main(String[] args) {
+
+//        system.out.println("Hello")
+        // above statement System instead of system, compile time error also semicolon is missing
+
+//        x= 90;  data type is not declare, compile time error
+
+
+    }
+}
