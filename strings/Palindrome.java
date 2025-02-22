2be29918ed9a2f5bb5c8ec64168668df86a74d7a
Nitish Kumar <nitish983577@gmail.com>
2025-02-22 15:29:10 +0530

    Palindrome Substrings

--- strings/Palindrome.java
+++ strings/Palindrome.java
@@ -1,2 +1,20 @@
-package Java_Program.strings;public class Palindrome {
+package Java_Program.strings;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str= "aba";
+        int i= 0;
+        int j= str.length()-1;
+        boolean flag= true;
+
+        while(i!=j){
+            if(str.charAt(i)!=str.charAt(j)){
+                System.out.println("Not Palindrome!");
+                return;
+            }
+            i++;
+            j--;
+        }
+        System.out.println("Palindrome");
+    }
 }
