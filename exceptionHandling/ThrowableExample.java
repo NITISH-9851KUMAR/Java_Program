3d7824aae916b3551fcbe6399651d96b8eff8779
Nitish Kumar <nitish983577@gmail.com>
2025-02-24 21:17:47 +0530

    Throwable Keyword

---
+++ exceptionHandling/ThrowableExample.java
@@ -0,0 +1,13 @@
+package Java_Program.exceptionHandling;
+
+public class ThrowableExample {
+    public static void main(String[] args) {
+        try{
+            int x= 10/0;
+        }catch (Throwable t) {
+            // use when we need to Catch both exceptions and errors
+            System.out.println("Error: " + t);
+        }
+
+    }
+}
