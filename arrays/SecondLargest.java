4396f7dc56bfc394078465cd09681545a478e4f1
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-03-01 18:41:49 +0530

    Second largest element within array

--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -1,2 +1,21 @@
-package Java_Program.arrays;public class SecondLargest {
+package Java_Program.arrays;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+
+        int val[]= {40, 30, 92, 50, 51, 35};
+        int fMax= val[0];
+        for(int i= 0; i<val.length; i++)
+            if(val[i]>fMax) fMax= val[i];
+
+        System.out.println("First Max: "+fMax);
+        int sMax= val[0];
+        for(int i= 0; i<val.length; i++){
+            if(val[i]== fMax) continue;
+            if(val[i]>sMax) sMax= val[i];
+        }
+        System.out.println("Second Max: " + sMax);
+
+
+    }
 }
