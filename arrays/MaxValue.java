be01a500afe5ec78c6aec1c4cbc5c31edecf04a6
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-03-01 18:41:19 +0530

    Max value

--- arrays/MaxValue.java
+++ arrays/MaxValue.java
@@ -1,2 +1,15 @@
-package Java_Program.arrays;public class MaxValue {
+package Java_Program.arrays;
+
+public class MaxValue {
+    public static void main(String[] args) {
+
+        int val[]= {40, 30, 92, 50, 51, 35};
+
+        int max= val[0];
+        for(int i= 0; i<val.length; i++)
+            if(val[i]>max) max= val[i];
+
+        System.out.println("Maximum Value: "+max);
+
+    }
 }
