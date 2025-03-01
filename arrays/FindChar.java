dd818e580341dc82807b65d134ff637a702a77d4
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-03-01 18:40:59 +0530

    Find char within array

--- arrays/FindChar.java
+++ arrays/FindChar.java
@@ -1,2 +1,16 @@
-package Java_Program.arrays;public class FindChar {
+package Java_Program.arrays;
+
+public class FindChar {
+    public static void main(String[] args) {
+
+        char[] arr= {'a', 'b', 'n', 'l', 'f', 'x', 't', 'z'};
+        char target_element= 'x';
+        for(int i= 0; i<arr.length; i++){
+            if(target_element== arr[i]){
+                System.out.println("Found at "+ i +" position");
+                return;
+            }
+        }
+
+    }
 }
