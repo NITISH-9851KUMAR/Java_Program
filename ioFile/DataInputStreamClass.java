107091ab689363676c7114e3e00f89efc4a4999e
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-03-06 17:48:14 +0530

    DataInputStream Class

--- ioFile/DataInputStreamClass.java
+++ ioFile/DataInputStreamClass.java
@@ -2,37 +2,38 @@ package Java_Program.ioFile;
 
 import java.io.DataInputStream;
 
-public class DataInputStreamClass {
-    public static void main(String[] args) {
+public class DataInputStreamClass{
+		
+		static float pAmount= 0.0f;
+		static float rate= 0.0f;
+		static float time= 0.0f;
+	public static void main(String[] args){
 
-        float pa;
-        float rate;
-        float time;
-        String tempString;
+		DataInputStream dis= new DataInputStream(System.in);
+		String temp;
 
-        try{
-            DataInputStream in= new DataInputStream(System.in);
-            System.out.print("Enter Principal Amount: ");
+		try{
+            System.out.print("Enter Principle Amount: ");
             System.out.flush();
-            tempString= in.readLine();
-            pa= Integer.parseInt(tempString);
-
-            System.out.print("Enter Rate: ");
-            System.out.flush();
-            rate= Integer.parseInt(tempString);
+            temp= dis.readLine();
+            pAmount= Float.parseFloat(temp);
 
             System.out.print("Enter Time: ");
             System.out.flush();
-            tempString= in.readLine();
-            time= Integer.parseInt(tempString);
+            temp= dis.readLine();
+            time= Float.parseFloat(temp);
 
-            float interest= (pa*time*rate)/100;
-            System.out.printf("Interest: %.2f",interest);
+            System.out.print("Enter Rate: ");
+            System.out.flush();
+            temp= dis.readLine();
+            rate= Float.parseFloat(temp);
 
+            float interest= (pAmount*time*rate)/100;
+            System.out.printf("Interest Amout: %.2f",interest);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+		
 
-
-    }
-}
+	} //main function terminated There
+}//Main Class Terminated There 
