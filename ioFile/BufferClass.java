6210b882a37170511213f7992a9005e89a5798dd
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-03-06 21:31:05 +0530

    BufferedOutputStream class

---
+++ ioFile/BufferClass.java
@@ -0,0 +1,20 @@
+package Java_Program.ioFile;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+
+public class BufferClass {
+    public static void main(String[] args) throws Exception {
+
+        FileOutputStream fOut= new FileOutputStream("N:\\File Handling/File.txt");
+        BufferedOutputStream bout= new BufferedOutputStream(fOut);
+
+        String s= "Welcome to NPTEL!";
+        byte b[]= s.getBytes();
+        bout.write(b);
+        bout.flush();
+        bout.close();
+        fOut.close();
+        System.out.println("Write to the file successfully");
+    }
+}
