27e9112a8c0cc3c18f3d4cfc9bcea713e695ef56
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-03-08 09:56:33 +0530

    Password Valid

---
+++ basics/PasswordCheck.java
@@ -0,0 +1,56 @@
+/*
+*  The password must be at least 8 characters
+*  The password must contain at least one uppercase letter
+*  The password must contain at least one numeric digit
+* */
+
+package Java_Program.basics;
+
+public class PasswordCheck {
+    private String password;
+    PasswordCheck(String pass){
+        this.password= pass;
+    }
+    boolean isValidPassword(String password){
+        this.password= password;
+        //Calculating the password length
+        int passLength= password.length();
+        boolean flagLength= false; // means length is less than 8
+        if(passLength >= 8){
+            flagLength= true;
+        }
+        //flagNum for checking if any number present in password
+        boolean flagNum= false; // false means number is not present
+
+        //flagChar for checking capital alphabet present in password
+        boolean flagChar= false; // means capital letter is not present
+
+        char ch;
+        int ascii= 0;
+        for(int i= 0; i<passLength; i++){
+            ch= password.charAt(i);
+            if(Character.isDigit(ch)){ // checking number
+                flagNum= true;
+                break;
+            }
+        }
+        for(int i= 0; i<passLength; i++){
+            ch= password.charAt(i);
+            if(Character.isUpperCase(ch)){ // check capital alphabet present or not
+                flagChar= true;
+                break;
+            }
+        }
+        if(flagLength && flagNum && flagChar){
+            return true;
+        }
+        else return false;
+    }
+    public static void main(String[] args) {
+
+        PasswordCheck pa= new PasswordCheck("Nitiya123");
+        if(pa.isValidPassword("nitiYa@9851prish")){
+            System.out.println("Password is valid ");
+        }else System.out.println("Password is invalid ");
+    }
+}
