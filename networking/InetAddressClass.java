8806af21541995cb41b181a2451e752e944bf06b
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-03-30 18:41:34 +0530

    InetAddress Class

---
+++ networking/InetAddressClass.java
@@ -0,0 +1,16 @@
+package Java_Program.networking;
+
+import java.net.InetAddress;
+public class InetAddressClass {
+    public static void main(String[] args) {
+        try{
+
+            InetAddress ip= InetAddress.getByName("www.youtube.com");
+            System.out.println("Host Name: "+ip.getHostName());
+            System.out.println("IP Address: "+ip.getHostAddress());
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
