b96757ef86ed236c343b909aa871628b441b7312
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-03-30 18:02:45 +0530

    URLConnection Class Example

---
+++ networking/URLConnectionClass.java
@@ -0,0 +1,32 @@
+package Java_Program.networking;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.MalformedURLException;
+import java.io.InputStream;
+public class URLConnectionClass {
+    public static void main(String[] args) {
+
+        try{
+            //create a Object of URL class
+            URL url= new URL("https://nptel.ac.in/course.php");
+
+            URLConnection urlCon= url.openConnection();
+            InputStream stream= urlCon.getInputStream();
+
+            int i;
+            while((i=stream.read())!=-1){
+                System.out.print((char)i);
+            }
+
+        }catch(MalformedURLException e){// catch related to URL class
+            System.out.println(e.getMessage());
+        }catch(IOException e1){ // for URLConnection class
+            System.out.println(e1.getMessage());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
