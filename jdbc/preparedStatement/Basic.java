3d93c5df2f60868c28587c685cc07aa6e7426fab
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-04-01 14:22:54 +0530

    Basics of PreparedStatement

---
+++ jdbc/preparedStatement/Basic.java
@@ -0,0 +1,33 @@
+package Java_Program.jdbc.preparedStatement;
+import java.sql.*;
+public class Basic {
+    private static final String url="jdbc:oracle:thin:@localHost:1521:XE";
+    private static final String userName="system";
+    private static final String password="nitish";
+    public static void main(String[] args) {
+        Connection connection= null;
+        PreparedStatement preparedStatement= null;
+
+        try{
+            connection=DriverManager.getConnection(url,userName,password);
+            String query= "";
+            preparedStatement= connection.prepareStatement(query); // compile query
+
+            preparedStatement.executeUpdate(); // execute query
+
+
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }finally {
+            try{
+                if(preparedStatement!=null)  // close the PreparedStatement connection
+                    preparedStatement.close();
+                if(connection!=null)  // close the connection
+                    connection.close();
+            }catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+}
