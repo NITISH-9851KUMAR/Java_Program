420f9cd5f289b41d3b636ad218904cdc0a614869
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-04-08 22:49:32 +0530

    rint Math function

--- methods/mathFunction/Rint.java
+++ methods/mathFunction/Rint.java
@@ -3,10 +3,11 @@ package Java_Program.methods.mathFunction;
 public class Rint {
     public static void main(String[] args) {
 
-        double x= Math.rint(90.23);
-        double y= Math.rint(90.51);
-// rint(x) Returns the truncated value of x
+        double x= Math.rint(90.83); // after point not 5 or 50 it is treated as simple function
+        double y= Math.rint(90.50);
+// rint(x) Returns the truncated value of x closet to even if .5 include
         System.out.println("rint of 90.23: "+x);
         System.out.println("rint of 90.51: "+y);
+        System.out.println("rint of 23.5: "+Math.rint(23.5)); // give the closet even number
     }
 }
