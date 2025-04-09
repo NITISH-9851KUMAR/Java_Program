f0533dbaf844c1af36660e769406bf472345a2e9
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-04-09 23:03:25 +0530

    Math function floor

--- methods/mathFunction/Floor.java
+++ methods/mathFunction/Floor.java
@@ -3,7 +3,7 @@ package Java_Program.methods.mathFunction;
 public class Floor {
     public static void main(String[] args) {
 
-        double num= Math.floor(23.23);
+        double num= Math.floor(23.90);
 //        ceil(x) Returns the largest whole number greater than or equal to x. (Rounded down)
 
         System.out.println("ceil of 23.23: "+num);
