095e4e471e430b235b3f864fa21019884128f9e0
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-04-09 13:31:24 +0530

    Date_Time Date API

---
+++ methods/DateTime/Method2.java
@@ -0,0 +1,12 @@
+/** java.util.Date, Time it is also older class. It has many issues, such as being mutable
+ * and difficulty to work  */
+package Java_Program.methods.DateTime;
+
+import java.util.Date;
+
+public class Method2 {
+    public static void main(String[] args) {
+        Date date= new Date();
+        System.out.println(date);
+    }
+}
