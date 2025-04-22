908d6b44e44b428db71766aedf26f3b26b76a1f7
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-04-22 21:57:09 +0530

    URL Class

--- networking/URLClass.java
+++ networking/URLClass.java
@@ -1,19 +1,22 @@
 package Java_Program.networking;
 
 import java.net.URL;
+import java.net.URI;
 import java.net.MalformedURLException;
 public class URLClass {
     public static void main(String[] args) {
 
         try{
+            URI uri= new URI("https://nptel.ac.in/course.php");
+
             // Declare a URL Object
-            URL url= new URL("https://nptel.ac.in/course.php");
+            URL url= uri.toURL();
 
             System.out.println("Protocol: "+url.getProtocol());
             System.out.println("Host Name: "+url.getHost());
             System.out.println("Port Number: "+url.getPort());
             System.out.println("File Name: "+url.getFile());
-        }catch(MalformedURLException e){
+        }catch(Exception e){
             // MalformedURLException generally used for URL class
             System.out.println(e.getMessage());
         }
